642a626812abf01b2ee25c095bdda10a8ef5ca30
oyhxgo <148951137+oyhxgo@users.noreply.github.com>
2023-10-25 16:22:58 +0800

    python
    text

---
+++ python/先自学/hello world.py
@@ -0,0 +1,24 @@
+# print("hello world")
+
+# a=1
+# b=1
+# c=1
+# print(a,b,c,sep='\n')
+
+
+# a=float(input())
+# b=float(input())
+# c=float(input())
+# s=a+b+c
+# s=s/3
+# print(s)
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# s=(a+b)*c/2
+# s=int(s)
+# print(s)
+a=int(input())
+b=int(input())
+print(b,a,sep='\n')
